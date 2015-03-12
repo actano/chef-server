207107f55dd88fcd05c2196840d42a742fc93e8d
Marcus Mennemeier <m.mennemeier@global-communication.de>
2015-03-12 10:34:51 +0100

    fix name, add depends to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,7 @@
-name 'config-chef-server'
+name 'actano-chef-server'
+description 'Install a simple chef-server instance'
 version '0.0.1'
+
+depends 'hostname'
+depends 'chef-server'
+depends 'iptables-ng'
